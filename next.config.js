41aafaf139e1887b874b313dbb8e90ef8c9981e6
AR Studio <arvercelcollab@gmail.com>
2026-06-17 17:57:43 +0200

    Anadir landing de membresias en /membresias y limpiar archivo mal ubicado

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: '/membresias', destination: '/membresias/index.html' },
+    ]
+  },
 }
 
 module.exports = nextConfig
